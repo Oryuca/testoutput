92cba6a825a68fa6eb03c356dc82039e852b540b
Oryuca <100742336+Oryuca@users.noreply.github.com>
2022-05-04 10:51:57 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = True,
+  name='oryuca1011'
+  version= '0.0.1',
+  description='oss-dev my calculator 1011',
+  author='hanyuongryu',
+  author_email='vf1011@naver.com',
+  url= "https://github.com/Oryuca/oryuc1011",
+  download_url = "",
+  install_requires=['pytest'],
+  long_description = 'oss-dec calculator python module',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+    "programming language :: python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
